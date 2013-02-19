d8ed5581c6e82c4119c8d5b3414beebbefe2336c
Asvel <fe.asvel@gmail.com>
2013-02-19 19:30:04 +0800

    完善时间映射解析

--- shifter.py
+++ shifter.py
@@ -7,19 +7,21 @@ subtitlefile = r"D:\temp\srh\test.ass"
 
 
 def parse_time(s):
-    l = len(s)
-    if l == 9:
-        time = datetime.datetime.strptime(s, "%M:%S.%f").time()
-    elif l == 12:
-        time = datetime.datetime.strptime(s, "%H:%M:%S.%f").time()
-    else:
+    time = None
+    for fmt in ["%M:%S.%f","%H:%M:%S.%f",]:
+        try:
+            time = datetime.datetime.strptime(s, fmt).time()
+            break
+        except:
+            pass
+    if time is None:
         raise Exception("无法解析时间 {}".format(s))
     return time
 
 timemap = []
 with open(timemapfile, encoding='utf-8') as f:
     for line in f:
-        timemap.append((parse_time(x) for x in line.split()))
+        timemap.append([parse_time(x) for x in line.split()])
 
 subs = pysubs.load(subtitlefile, encoding='utf-8')
 for line in subs:
