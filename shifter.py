0f19ed911ee659c951c9f4dc60ea318c7f751ba6
Asvel <fe.asvel@gmail.com>
2013-02-20 21:15:59 +0800

    重构代码，添加日志

--- shifter.py
+++ shifter.py
@@ -1,11 +1,15 @@
+# -*- coding: utf-8 -*-
 
 import datetime
+import logging
+
 import pysubs
 from pysubs import Time
 
 timemapfile = r"D:\temp\srh\timemap.txt"
 fni = r"D:\temp\srh\test.ass"
 fno = r"D:\temp\srh\testo.ass"
+
 
 def parse_time(s):
     t = None
@@ -19,26 +23,40 @@ def parse_time(s):
         raise Exception("无法解析时间 {}".format(s))
     return Time(h=t.hour, m=t.minute, s=t.second, ms=t.microsecond//1000)
 
+def shift(subs, offsets):
+    for line in subs:
+        timemap_start = next(x for x in offsets if line.start < x['until'])
+        timemap_end = next(x for x in offsets if line.end < x['until'])
+        if timemap_start != timemap_end:
+            logging.warning("字幕 {} 的开始时间与结束时间对应不同的时间映射"
+                            .format(str(line)))
+            raise Exception("timemap_start != timemap_end")
+        line.start += timemap_start['delta']
+        if line.start < Time.zero:
+            line.start = Time.zero
+            logging.warning("字幕 {} 的开始时间调整后小于0".format(str(line)))
+        line.end += timemap_end['delta']
+        if line.end < Time.zero:
+            line.end = Time.zero
+            logging.warning("字幕 {} 的结束时间调整后小于0".format(str(line)))
+
+def generate_timedelta(timemap):
+    timemap = [x for x in timemap if 1 <= len(x) <= 2] + [[Time(h=100)]]
+    offsets = []
+    for i in range(len(timemap)-1):
+        if len(timemap[i]) == 2:
+            offsets.append({'delta': timemap[i][1] - timemap[i][0],
+                'until': timemap[i+1][0]})
+    return offsets
+
+logging.basicConfig(format='%(levelname)s\t: %(message)s', level=logging.DEBUG)
+
 timemap = []
 with open(timemapfile, encoding='utf-8') as f:
     for line in f:
         timemap.append([parse_time(x) for x in line.split()])
 
-timemap = [x for x in timemap if 1 <= len(x) <= 2]
-timemap.append([Time(h=100)])
-for i in range(len(timemap)-1):
-    if len(timemap[i]) == 2:
-        timemap[i][1] -= timemap[i][0]  # offset
-        timemap[i][0] = timemap[i+1][0] # until
-timemap = [x for x in timemap if len(x) == 2]
-
 subs = pysubs.load(fni, encoding='utf-8')
-for line in subs:
-    timemap_start = next(x for x in timemap if x[0] > line.start)
-    timemap_end = next(x for x in timemap if x[0] > line.end)
-    if timemap_start != timemap_end:
-        raise Exception("timemap_start != timemap_end")
-    line.start += timemap_start[1]
-    line.end += timemap_end[1]
-
+timedelta = generate_timedelta(timemap)
+shift(subs, timedelta)
 subs.save(fno)
