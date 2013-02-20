208a017b746517b799780a78f6d06a11d3ee6096
Asvel <fe.asvel@gmail.com>
2013-02-20 17:05:56 +0800

    按照时间映射调轴的功能基本完成

--- shifter.py
+++ shifter.py
@@ -1,28 +1,44 @@
 
 import datetime
 import pysubs
+from pysubs import Time
 
 timemapfile = r"D:\temp\srh\timemap.txt"
-subtitlefile = r"D:\temp\srh\test.ass"
-
+fni = r"D:\temp\srh\test.ass"
+fno = r"D:\temp\srh\testo.ass"
 
 def parse_time(s):
-    time = None
+    t = None
     for fmt in ["%M:%S.%f","%H:%M:%S.%f",]:
         try:
-            time = datetime.datetime.strptime(s, fmt).time()
+            t = datetime.datetime.strptime(s, fmt).time()
             break
         except:
             pass
-    if time is None:
+    if t is None:
         raise Exception("无法解析时间 {}".format(s))
-    return time
+    return Time(h=t.hour, m=t.minute, s=t.second, ms=t.microsecond//1000)
 
 timemap = []
 with open(timemapfile, encoding='utf-8') as f:
     for line in f:
         timemap.append([parse_time(x) for x in line.split()])
 
-subs = pysubs.load(subtitlefile, encoding='utf-8')
+timemap = [x for x in timemap if 1 <= len(x) <= 2]
+timemap.append([Time(h=100)])
+for i in range(len(timemap)-1):
+    if len(timemap[i]) == 2:
+        timemap[i][1] -= timemap[i][0]  # offset
+        timemap[i][0] = timemap[i+1][0] # until
+timemap = [x for x in timemap if len(x) == 2]
+
+subs = pysubs.load(fni, encoding='utf-8')
 for line in subs:
-    print(line)
+    timemap_start = next(x for x in timemap if x[0] > line.start)
+    timemap_end = next(x for x in timemap if x[0] > line.end)
+    if timemap_start != timemap_end:
+        raise Exception("timemap_start != timemap_end")
+    line.start += timemap_start[1]
+    line.end += timemap_end[1]
+
+subs.save(fno)
