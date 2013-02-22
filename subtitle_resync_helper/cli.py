b145128926d22be3a0ff60669519aebc1e50581a
Asvel <fe.asvel@gmail.com>
2013-02-22 20:16:21 +0800

    添加命令行接口

---
+++ subtitle_resync_helper/cli.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import argparse
+
+import pysubs
+
+from subtitle_resync_helper import time, timemap, shifter
+
+
+def run():
+
+    class ArgumentParser(argparse.ArgumentParser):
+        def format_usage(self):
+            return argparse.ArgumentParser.format_usage(self) \
+                .replace("usage:", "用法：")
+        def format_help(self):
+            return argparse.ArgumentParser.format_help(self) \
+                .replace("usage:", "用法：") \
+                .replace("positional arguments:", "参数：") \
+                .replace("\n\noptional arguments:", "") \
+                .replace("show this help message and exit", "显示此帮助并退出")
+
+    parser = ArgumentParser(
+        description='根据时间映射表调整字幕时间轴',
+        epilog="""\
+""",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('timemap', help="时间映射表文件名")
+    parser.add_argument('source', help="原始字幕文件")
+    parser.add_argument('result', help="调整后的字幕文件")
+    parser.add_argument('-l', '--loglevel', default='info',
+                        choices=['none', 'error', 'warning', 'info', 'debug'],
+                        help="输出信息的等级，默认为 info")
+    parser.add_argument('-d', '--diffdeltahandle', default='apart',
+                        choices=['stop', 'apart', 'start', 'end'],
+                        help="开始结束时间调整量不同的处理方法，默认为 apart")
+    parser.add_argument('-ec', '--endcheck', action="store_true",
+                        help="时间映射表结尾存在校验项")
+    args = parser.parse_args()
+
+    ftm = args.timemap
+    fni = args.source
+    fno = args.result
+
+    loglevel = getattr(logging, args.loglevel.upper(), logging.CRITICAL)
+    logging.basicConfig(format='%(levelname)s\t: %(message)s', level=loglevel)
+
+    diffdeltahandledict = {
+        'stop':shifter.SHIFT_STOP,
+        'apart':shifter.SHIFT_APART,
+        'start':shifter.SHIFT_BY_START,
+        'end':shifter.SHIFT_BY_END,
+    }
+    diffdeltahandle = diffdeltahandledict[args.diffdeltahandle]
+
+    endcheck = args.endcheck
+
+    tmap = []
+    with open(ftm, encoding='utf-8') as f:
+        for line in f:
+            tmap.append([time.parse(x) for x in line.split()])
+
+    timedelta = timemap.normalize(tmap, endcheck)
+
+    subs = pysubs.load(fni)
+    shifter.shift(subs, timedelta, diffdeltahandle)
+    subs.save(fno)
