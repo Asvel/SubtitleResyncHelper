72fe4e912eaef121e5e258f2d34896b65f22c99d
Asvel <fe.asvel@gmail.com>
2013-02-22 19:13:13 +0800

    添加时间映射表的结尾校验功能

--- subtitle_resync_helper/timemap.py
+++ subtitle_resync_helper/timemap.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import datetime
+import logging
 
 from .time import Time
 
 
-def normalize(timemap):
+def normalize(timemap, endcheck=False):
     timemap = [x for x in timemap if 1 <= len(x) <= 2] + [[Time(h=100)]]
     timedelta = []
     for i in range(len(timemap)-1):
         if len(timemap[i]) == 2:
             timedelta.append({'delta': timemap[i][1] - timemap[i][0],
                               'until': timemap[i+1][0]})
+    if endcheck:
+        if timedelta[-1]['delta'] == timedelta[-2]['delta']:
+            del timedelta[-2]
+        else:
+            logging.error("结尾校验没有通过，可能存在遗漏的时间映射")
+            raise Exception("结尾校验没有通过")
     return timedelta
