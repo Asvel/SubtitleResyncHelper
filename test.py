fefd5680d09f038b19152be7563db4b6722cc498
Asvel <fe.asvel@gmail.com>
2013-02-26 22:27:14 +0800

    添加向后台窗口发送按键的功能

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 
 from PyQt4.QtGui import QApplication
 
 from subtitle_resync_helper import config
 config.playerpath = r'C:\app\MPC-BE\mpc-be.exe'
 config.playername = "MPCBE"
-
+"""
 from subtitle_resync_helper import gui
 
 app = QApplication(sys.argv)
@@ -15,3 +16,16 @@ window = gui.FormTimemapper(r"D:\temp\srh\src.mkv", r"D:\temp\srh\dst.mkv")
 
 window.show()
 sys.exit(app.exec())
+"""
+
+from PyQt4.QtGui import QKeySequence
+from subtitle_resync_helper import player, win
+Player = player.getplayer(config.playername)
+
+p = Player(r"D:\temp\srh\dst.mkv")
+#p.grabtime()
+#input()
+
+time.sleep(1)
+win.SendKey(p._mainhwnd, "Ctrl+G")
+input()
