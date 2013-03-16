44820f9eb693e97918c68f7c1e7600a5ad8004ed
Asvel <fe.asvel@gmail.com>
2013-03-16 17:33:48 +0800

    配置存储到磁盘

--- subtitle_resync_helper/config.py
+++ subtitle_resync_helper/config.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import os as _os
+import sys as _sys
+import logging as _logging
+import json as _json
+
+
 playerpath = ""
 playername = None
 
@@ -7,3 +13,27 @@ filedialog_lastdir = ""
 
 fileext_video = ["mkv", "mp4"]
 fileext_subtitle = ["ass", "ssa", "srt"]
+
+_config_path = _os.path.join(_os.path.dirname(_sys.argv[0]), "config.json")
+
+
+def _load():
+    if _os.path.exists(_config_path):
+        with open(_config_path, encoding='utf-8') as f:
+            config = _json.load(f)
+        global_ = globals()
+        for k in global_:
+            if not k.startswith('_') and k in config:
+                global_[k] = config[k]
+    else:
+        _logging.warning("配置文件 {} 不存在，使用默认配置".format(
+            _config_path))
+_load()
+
+def _save():
+    config = {}
+    for k, v in globals().items():
+        if not k.startswith('_'):
+            config[k] = v
+    with open(_config_path, mode='w', encoding='utf-8') as f:
+        _json.dump(config, f, ensure_ascii=False, indent='\t', sort_keys=True)
--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import time
 
 from PyQt4.QtGui import QApplication
 
-from subtitle_resync_helper import config
-config.playerpath = r'C:\app\MPC-BE\mpc-be.exe'
-config.playername = "MPCBE"
-
-from subtitle_resync_helper import gui
+from subtitle_resync_helper import config, gui
 
 app = QApplication(sys.argv)
 window = gui.FormMain()
 
 window.show()
-sys.exit(app.exec())
+ret = app.exec()
+
+config._save()
+
+sys.exit(ret)
