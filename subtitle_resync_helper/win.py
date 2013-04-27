40319c19f44fca679cf4ce8353b1a5d1d4fab784
Asvel <fe.asvel@gmail.com>
2013-04-27 21:30:20 +0800

    移除 SendKey

--- subtitle_resync_helper/win.py
+++ subtitle_resync_helper/win.py
@@ -49,118 +49,6 @@ def GetWindowTextX(hwnd):
     return buffer.value
 
 
-_qtkeycodemap = {
-    int(Qt.Key_Escape): VK_ESCAPE,
-    int(Qt.Key_Tab): VK_TAB,
-    int(Qt.Key_Backtab): VK_TAB,
-    int(Qt.Key_Backspace): VK_BACK,
-    int(Qt.Key_Return): VK_RETURN,
-    int(Qt.Key_Enter): VK_RETURN,
-    int(Qt.Key_Insert): VK_INSERT,
-    int(Qt.Key_Delete): VK_DELETE,
-    int(Qt.Key_Pause): VK_PAUSE,
-    int(Qt.Key_Print): VK_PRINT,
-    int(Qt.Key_Clear): VK_CLEAR,
-    int(Qt.Key_Home): VK_HOME,
-    int(Qt.Key_End): VK_END,
-    int(Qt.Key_Left): VK_LEFT,
-    int(Qt.Key_Up): VK_UP,
-    int(Qt.Key_Right): VK_RIGHT,
-    int(Qt.Key_Down): VK_DOWN,
-    int(Qt.Key_PageUp): VK_PRIOR,
-    int(Qt.Key_PageDown): VK_NEXT,
-    int(Qt.Key_F1): VK_F1,
-    int(Qt.Key_F2): VK_F2,
-    int(Qt.Key_F3): VK_F3,
-    int(Qt.Key_F4): VK_F4,
-    int(Qt.Key_F5): VK_F5,
-    int(Qt.Key_F6): VK_F6,
-    int(Qt.Key_F7): VK_F7,
-    int(Qt.Key_F8): VK_F8,
-    int(Qt.Key_F9): VK_F9,
-    int(Qt.Key_F10): VK_F10,
-    int(Qt.Key_F11): VK_F11,
-    int(Qt.Key_F12): VK_F12,
-    int(Qt.Key_F13): VK_F13,
-    int(Qt.Key_F14): VK_F14,
-    int(Qt.Key_F15): VK_F15,
-    int(Qt.Key_F16): VK_F16,
-    int(Qt.Key_F17): VK_F17,
-    int(Qt.Key_F18): VK_F18,
-    int(Qt.Key_F19): VK_F19,
-    int(Qt.Key_F20): VK_F20,
-    int(Qt.Key_F21): VK_F21,
-    int(Qt.Key_F22): VK_F22,
-    int(Qt.Key_F23): VK_F23,
-    int(Qt.Key_F24): VK_F24,
-    int(Qt.Key_Space): VK_SPACE,
-    int(Qt.Key_Asterisk): VK_MULTIPLY,
-    int(Qt.Key_Plus): VK_ADD,
-    int(Qt.Key_Comma): VK_SEPARATOR,
-    int(Qt.Key_Minus): VK_SUBTRACT,
-    int(Qt.Key_Slash): VK_DIVIDE,
-    int(Qt.Key_MediaNext): VK_MEDIA_NEXT_TRACK,
-    int(Qt.Key_MediaPrevious): VK_MEDIA_PREV_TRACK,
-    int(Qt.Key_MediaPlay): VK_MEDIA_PLAY_PAUSE,
-    int(Qt.Key_MediaStop): 0xB2, # VK_MEDIA_STOP,
-    int(Qt.Key_VolumeDown): VK_VOLUME_DOWN,
-    int(Qt.Key_VolumeUp): VK_VOLUME_UP,
-    int(Qt.Key_VolumeMute): VK_VOLUME_MUTE,
-    int(Qt.Key_0): int(Qt.Key_0),
-    int(Qt.Key_1): int(Qt.Key_1),
-    int(Qt.Key_2): int(Qt.Key_2),
-    int(Qt.Key_3): int(Qt.Key_3),
-    int(Qt.Key_4): int(Qt.Key_4),
-    int(Qt.Key_5): int(Qt.Key_5),
-    int(Qt.Key_6): int(Qt.Key_6),
-    int(Qt.Key_7): int(Qt.Key_7),
-    int(Qt.Key_8): int(Qt.Key_8),
-    int(Qt.Key_9): int(Qt.Key_9),
-    int(Qt.Key_A): int(Qt.Key_A),
-    int(Qt.Key_B): int(Qt.Key_B),
-    int(Qt.Key_C): int(Qt.Key_C),
-    int(Qt.Key_D): int(Qt.Key_D),
-    int(Qt.Key_E): int(Qt.Key_E),
-    int(Qt.Key_F): int(Qt.Key_F),
-    int(Qt.Key_G): int(Qt.Key_G),
-    int(Qt.Key_H): int(Qt.Key_H),
-    int(Qt.Key_I): int(Qt.Key_I),
-    int(Qt.Key_J): int(Qt.Key_J),
-    int(Qt.Key_K): int(Qt.Key_K),
-    int(Qt.Key_L): int(Qt.Key_L),
-    int(Qt.Key_M): int(Qt.Key_M),
-    int(Qt.Key_N): int(Qt.Key_N),
-    int(Qt.Key_O): int(Qt.Key_O),
-    int(Qt.Key_P): int(Qt.Key_P),
-    int(Qt.Key_Q): int(Qt.Key_Q),
-    int(Qt.Key_R): int(Qt.Key_R),
-    int(Qt.Key_S): int(Qt.Key_S),
-    int(Qt.Key_T): int(Qt.Key_T),
-    int(Qt.Key_U): int(Qt.Key_U),
-    int(Qt.Key_V): int(Qt.Key_V),
-    int(Qt.Key_W): int(Qt.Key_W),
-    int(Qt.Key_X): int(Qt.Key_X),
-    int(Qt.Key_Y): int(Qt.Key_Y),
-    int(Qt.Key_Z): int(Qt.Key_Z),
-}
-def SendKey(hwnd, key):
-    key = QKeySequence(key)[0]
-    ctrl = bool(key & 0x04000000)
-    alt = bool(key & 0x08000000)
-    shift = bool(key & 0x02000000)
-    win = bool(key & 0x10000000)
-    key &= 0x1FFFFFF
-    if ctrl: keybd_event(VK_CONTROL, 0, 0, 0)
-    if alt: keybd_event(VK_MENU, 0, 0, 0)
-    if shift: keybd_event(VK_SHIFT, 0, 0, 0)
-    if win: keybd_event(VK_LWIN, 0, 0, 0)
-    PostMessage(hwnd, WM_KEYDOWN, _qtkeycodemap[key], 0)
-    if ctrl: keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0)
-    if alt: keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0)
-    if shift: keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0)
-    if win: keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0)
-
-
 def GetTopLevelWindows():
     windows = []
     def EnumWindowProc(hwnd, lparam):
@@ -211,6 +99,7 @@ def FindWindows(class_ = None, title = None, parent = None, process = None,
         windows = [x for x in windows if try_(lambda:IsWindowEnabled(x))]
     return windows
 
+
 class COPYDATASTRUCT(Structure):
     _fields_ = [
         ('dwData', LPARAM),
@@ -218,12 +107,10 @@ class COPYDATASTRUCT(Structure):
         ('lpData', c_void_p)
     ]
 PCOPYDATASTRUCT = ctypes.POINTER(COPYDATASTRUCT)
-
 def CopyData_SendString(hwnd, dwData, lpData=""):
     data = create_unicode_buffer(lpData)
     CDS = COPYDATASTRUCT(dwData, sizeof(data), cast(data, c_void_p))
     SendMessage(hwnd, WM_COPYDATA, 0, addressof(CDS))
-
 def CopyData_ParseString(lParam):
     pCDS = cast(lParam, PCOPYDATASTRUCT)
     dwData = pCDS.contents.dwData
