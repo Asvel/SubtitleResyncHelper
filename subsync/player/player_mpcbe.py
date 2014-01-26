1f4766f86db1e76bc98a3acbd7de770dcc5bceca
Asvel <fe.asvel@gmail.com>
2014-01-26 15:08:15 +0800

    移除已经不需要了的 MPC-BE 相对于 MPC-HC 的特殊处理

--- subsync/player/player_mpcbe.py
+++ subsync/player/player_mpcbe.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from subsync import config, win
 from subsync.player.player_mpchc import PlayerMPCHC as Player
-from subsync.player.player_mpchc import CMD
 
 
 class PlayerMPCBE(Player):
-
-    def _open(self):
-        super(PlayerMPCBE, self)._open()
-        self._first_paused = False
-        self._send_message(CMD.OPENFILE, self._filepath)
-
-    def _generate_args(self):
-        return [config.playerpath, "/new", "/slave", str(self._hwnd_listener)]
-
-    def _parse_message(self, command, message):
-        if command == CMD.PLAYMODE:
-            if message == "0" and not self._first_paused:
-                self._first_paused = True
-                self._send_message(CMD.PLAYPAUSE)
-        else:
-            super(PlayerMPCBE, self)._parse_message(command, message)
+    pass
